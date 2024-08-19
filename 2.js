e318db01fae849f68f68ae46e291382ac0d13837
Dobromira Shtereva <dobi.shtereva@gmail.com>
2024-08-19 16:58:18 +0300

    Add files via upload

---
+++ 2.js
@@ -0,0 +1,17 @@
+function demo() {
+    let color = 'green';
+
+    switch (color) {
+        case 'blue': console.log('the sky is blue');
+            break;
+        case 'green': console.log('the grass is green');
+            break;
+        case 'yellow': console.log('the sun is wellow');
+            break;
+        default:
+            console.log('different color');
+            break;
+
+    }
+}
+demo()
